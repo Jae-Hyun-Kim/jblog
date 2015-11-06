f0ce977dd6c7c2c0c08b0f1279ee9e49cc6786c1
bit-user <bit-user@bit>
2015-11-06 16:27:24 +0900

    1234

--- src/main/java/com/bit2015/jblog/vo/CommentsVo.java
+++ src/main/java/com/bit2015/jblog/vo/CommentsVo.java
@@ -1,10 +1,10 @@
 package com.bit2015.jblog.vo;
 
 public class CommentsVo {
-	COMMENTS_NO NUMBER(10) NOT NULL ,
+	/*COMMENTS_NO NUMBER(10) NOT NULL ,
 	POST_NO NUMBER(10) NOT NULL,
 	CONTENT VARCHAR2(1000),
 	CREATED_DATE DATE,
 	private Long commentsNo;
-	private Long po
+	private Long po*/
 }
